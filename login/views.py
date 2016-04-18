b52ebccfd648f4da64c8c0f9e01febbc6e076451
Chris Kyle <ckyle@ucsc.edu>
2016-04-18 01:05:41 -0700

    Implement basic google authentication

--- login/views.py
+++ login/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from django.conf import settings
+import os
 
 def index(request):
-    return render(request, 'login/index.html')
+    try:
+        with open('key_file.txt', 'r') as f:
+            GOOGLE_KEY = f.read().rstrip()
+    except IOError:
+        GOOGLE_KEY = None
+        print("key_file not found. \nMessage Ckyle for key_file.txt")
+    context = {'GOOGLE_KEY' : GOOGLE_KEY }
+    return render(request, 'login/index.html', context)
