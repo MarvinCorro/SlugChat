2cb1d2ef726868ca9bb72fb02b38d9ef07b76801
Marvin Corro <corro.marvin@gmail.com>
2016-04-26 00:02:34 -0700

    Implemented better containers for a more responsive site

--- templates/custom-script.js
+++ templates/custom-script.js
@@ -1,45 +1,50 @@
-$(function(){
-	var slideqty = $('#featured .item').length;
- 	var wheight = $(window).height(); //get the height of the window
+$(function()
+{
+    var slideqty = $('#featured .item').length;
+    var wheight = $(window).height(); //get the height of the window
 
 
-  $('.fullheight').css('height', wheight); //set to window tallness  
-
-
-  //replace IMG inside carousels with a background image
-  $('#featured .item img').each(function() {
-    var imgSrc = $(this).attr('src');
-    $(this).parent().css({'background-image': 'url('+imgSrc+')'});
-    $(this).remove();
-  });
-
-  //adjust height of .fullheight elements on window resize
-  $(window).resize(function() {
-    wheight = $(window).height(); //get the height of the window
     $('.fullheight').css('height', wheight); //set to window tallness  
-  });
-
-  for (var i=0; i < slideqty; i++) {
-    var insertText = '<li data-target="#featured" data-slide-to="' + i + '"';
-    insertText += '></li>';
-    $('#featured ol').append(insertText);
-    //alert(insertText);
-  }
 
 
-  $('.carousel').carousel({
-    pause: 'none'
-  });  
+    //replace IMG inside carousels with a background image
+    $('#featured .item img').each(function()
+    {
+        var imgSrc = $(this).attr('src');
+        $(this).parent().css(
+        {
+            'background-image': 'url(' + imgSrc + ')'
+        });
+        $(this).remove();
+    });
 
-  $('#featured').carousel({
-    interval: 7000,
-    pause: "false"
-});
+    //adjust height of .fullheight elements on window resize
+    $(window).resize(function()
+    {
+        wheight = $(window).height(); //get the height of the window
+        $('.fullheight').css('height', wheight); //set to window tallness  
+    });
 
-$(window).resize(function(){
-  $(".home-page-content").height( ($( window ).height()) - ($(".main-header").height()) - ($(".main-footer").height()));
-});
-$(window).ready(function(){
-  $(".home-page-content").height( ($( window ).height()) - ($(".main-header").height()) - ($(".main-footer").height()));
-});
+    for (var i = 0; i < slideqty; i++)
+    {
+        var insertText = '<li data-target="#featured" data-slide-to="' + i + '"';
+        insertText += '></li>';
+        $('#featured ol').append(insertText);
+    }
+
+    $('#featured').carousel(
+    {
+        interval: 7000,
+        pause: "false"
+    });
+
+    $(window).resize(function()
+    {
+        $(".home-page-content").height(($(window).height()) - ($(".main-header").height()) - ($(".main-footer").height()));
+    });
+
+    $(window).ready(function()
+    {
+        $(".home-page-content").height(($(window).height()) - ($(".main-header").height()) - ($(".main-footer").height()));
+    });
 });
