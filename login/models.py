fba05eb53a03ec61738db59308a5b23ad5b4ceae
CJ <cketchum@ucsc.edu>
2016-04-28 17:11:35 -0700

    Preliminary database schemas

--- login/models.py
+++ login/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+	TA = "TA"
+	PROFESSOR = "PR"
+	STUDENT = "ST"
+	statusChoices = (
+		(TA, 'Teaching Assistant'),
+		(PROFESSOR, 'Professor'),
+		(STUDENT, 'Student'),
+	)
+	username = models.CharField(max_length=50)
+	firstName = models.CharField(max_length=50)
+	lastName = models.CharField(max_length=50)
+	school = models.CharField(max_length=50)
+	studentID = models.CharField(max_length=10)
+	email = models.CharField(max_length=50)
+	status = models.CharField(max_length=2, choices=statusChoices, default=STUDENT)
+
+
+class Course(models.Model):
+	# these field points to tuple in User DB
+	professor = models.ForeignKey(
+		'User',
+		on_delete=models.CASCADE
+	)
+	ta = models.ForeignKey(
+		'User',
+		on_delete=models.CASCADE
+	)
+	school = models.CharField(max_length=50)
+	title = models.CharField(max_length=50)
+
+class Roster(models.Model):
+	studentID = models.ForeignKey(
+		'User',
+		on_delete=models.CASCADE
+	)
+	courseID = models.ForeignKey(
+		'Course',
+		on_delete=models.CASCADE
+	)
