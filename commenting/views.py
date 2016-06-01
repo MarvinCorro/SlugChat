658aac8c6fe8575714ed17b87e2185f1da8cde43
AramisM <amarden@ucsc.edu>
2016-05-31 20:14:20 -0700

    edit detail function

--- commenting/views.py
+++ commenting/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from datetime import datetime
 
-#from .forms import CommentForm
+#from commenting.forms import CommentForm
 from .models import File
 
 def index(request):
@@ -14,5 +15,13 @@ def index(request):
 def detail(request, file_id):
 	#fetches an object from the list of objects
 	file = get_object_or_404(File, pk=file_id)
-	#form = CommentForm()
-	return render(request, 'commenting/detail.html', {'file':file})
+	if request.method == 'POST':
+	        form = CommentForm(request.POST)
+	        if form.is_valid():
+			comment = form.save(commit=False)
+			comment.pub_date = datetime.now
+			comment.save()
+	else:
+		form = CommentForm()
+	return render(request, 'commenting/detail.html', {'file':file, 'form': form})
+
