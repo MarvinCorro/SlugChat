f372376213d78650981d5374353283dd38e018ab
Chris Kyle <ckyle@ucsc.edu>
2016-06-02 16:26:40 -0700

    Remove tabs from commenting/views.py

--- commenting/views.py
+++ commenting/views.py
@@ -5,23 +5,24 @@ from datetime import datetime
 from commenting.forms import CommentForm
 from .models import File
 
-def index(request):
-	#gets a list of all the objects that are commentable
-	file_list = File.objects.all()
-	#puts object list in a context
-	context = {'file_list': file_list,}
-	return render(request, 'commenting/index.html', context)
-    
-def detail(request, file_id):
-	#fetches an object from the list of objects
-	file = get_object_or_404(File, pk=file_id)
-	if request.method == 'POST':
-	        form = CommentForm(request.POST)
-	        if form.is_valid():
-			comment = form.save(commit=False)
-			comment.pub_date = datetime.now
-			comment.save()
-	else:
-		form = CommentForm()
-	return render(request, 'commenting/detail.html', {'file':file, 'form': form})
 
+def index(request):
+    #gets a list of all the objects that are commentable
+    file_list = File.objects.all()
+    #puts object list in a context
+    context = {'file_list': file_list,}
+    return render(request, 'commenting/index.html', context)
+
+
+def detail(request, file_id):
+    #fetches an object from the list of objects
+    file = get_object_or_404(File, pk=file_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.pub_date = datetime.now
+            comment.save()
+    else:
+        form = CommentForm()
+    return render(request, 'commenting/detail.html', {'file':file, 'form': form})
